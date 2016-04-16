506963b39b1b85b13ce19bb8a1c0703ebca01c75
@Lord_Coops <cooper@coopers-imac-2.home>
2016-04-16 15:58:52 +0100

    super fast chain reaction

--- src/play.js
+++ src/play.js
@@ -21,6 +21,7 @@
 */
 
 
+var ROTATTE_SPEED = 600
 var ACCELERATOR_SPEED = 800
 var REACTION_SPEED = ACCELERATOR_SPEED
 
@@ -45,14 +46,14 @@ playState.prototype = {
         PHASER.load.image('element-y', 'assets/imgs/element-y.png')
         PHASER.load.image('element-v', 'assets/imgs/element-v.png')
 
-        // mapping table
+        self.score = 0
         self.combinations = {
-            'element1-0+element1-0': { score: 2, elements: ['element1-1', 'element-v', 'element-p'] },
-            'element1-1+element1-0': { score: 2, elements: ['element2-1', 'element-y'] },
-            'element2-1+element2-2': { score: 2, elements: ['element4-3', 'element-y'] },
-            'element4-3+element1-0': { score: 10, elements: ['element5-3', 'element-y'] },
-            'element2-1+element2-1': { score: 2, elements: ['element1-0', 'element1-0', 'element2-2'] },
-            'element3-4+element1-0': { score: 2, elements: ['element2-2', 'element2-2'] },
+            'element1-0+element1-0': { score: 200, elements: ['element1-1', 'element-v', 'element-p'] },
+            'element1-1+element1-0': { score: 200, elements: ['element2-1', 'element-y'] },
+            'element2-1+element2-2': { score: 200, elements: ['element4-3', 'element-y'] },
+            'element4-3+element1-0': { score: 1000, elements: ['element5-3', 'element-y'] },
+            'element2-1+element2-1': { score: 200, elements: ['element1-0', 'element1-0', 'element2-2'] },
+            'element3-4+element1-0': { score: 200, elements: ['element2-2', 'element2-2'] },
         }
         self.elementsInfo = {
             'element1-0': { speed: { min: 10, max: 20 }, initialCount: 50 },
@@ -61,8 +62,8 @@ playState.prototype = {
             'element2-2': { speed: { min: 4, max: 14 } },
             'element4-3': { speed: { min: 2, max: 12 }, decay: { min: 3000, max: 5000, elements: ['element3-4', 'element-v'] } },
             'element3-4': { speed: { min: 2, max: 12 } },
-            'element4-4': { speed: { min: 1, max: 10 }, selectable: false, decay: { min: 3000, max: 5000, elements: ['element2-2', 'element2-2'] } },
-            'element5-3': { speed: { min: 0, max: 8 }, selectable: false, decay: { min: 2000, max: 3000, elements: ['element4-4', 'element-v', 'element-p'] } },
+            'element4-4': { speed: { min: 1, max: 10 }, selectable: false, decay: { min: 3000, max: 5000, score: 200, elements: ['element2-2', 'element2-2'] } },
+            'element5-3': { speed: { min: 0, max: 8 }, selectable: false, decay: { min: 2000, max: 3000, score: 200, elements: ['element4-4', 'element-v', 'element-p'] } },
 
             'element-p': { speed: { min: 2000, max: 2200 }, hip: true, selectable: false },
             'element-y': { speed: { min: 1800, max: 2000 }, hip: true, selectable: false },
@@ -108,19 +109,19 @@ playState.prototype = {
         PHASER.debug.text(PHASER.time.fps || '--', 2, 14, "#00ff00")
     },
 
-    addElement: function (elementType, x, y) {
+    addElement: function (elementType, x, y, reaction) {
         var self = this
         if (x == undefined || y == undefined) {
             x = PHASER.rnd.integerInRange(0, PHASER.world.width)
             y = PHASER.rnd.integerInRange(0, PHASER.world.height)
         }
-        var fx = PHASER.rnd.integerInRange(0, 1)
-        if (fx == 0) fx = -1
-        var fy = PHASER.rnd.integerInRange(0, 1)
-        if (fy == 0) fy = -1
         var elementInfo = self.elementsInfo[elementType]
-        var dx = PHASER.rnd.integerInRange(elementInfo.speed.min, elementInfo.speed.max) * fx
-        var dy = PHASER.rnd.integerInRange(elementInfo.speed.min, elementInfo.speed.max) * fy
+        var dx = PHASER.rnd.integerInRange(-ACCELERATOR_SPEED, ACCELERATOR_SPEED)
+        var dy = PHASER.rnd.integerInRange(-ACCELERATOR_SPEED, ACCELERATOR_SPEED)
+        if (reaction == undefined) {
+            dx = PHASER.rnd.integerInRange(-elementInfo.speed.min, elementInfo.speed.max)
+            dy = PHASER.rnd.integerInRange(-elementInfo.speed.min, elementInfo.speed.max)
+        }
         var element = PHASER.make.bitmapData()
         
         element.load(elementType)
@@ -132,15 +133,20 @@ playState.prototype = {
         sprite.anchor.set(0.5, 0.5)
         sprite.body.collideWorldBounds = true
         sprite.body.velocity.set(dx, dy)
-        sprite.body.angularVelocity = PHASER.rnd.realInRange(-600, 600)
+        sprite.body.angularVelocity = PHASER.rnd.realInRange(-ROTATTE_SPEED, ROTATTE_SPEED)
         sprite.inputEnabled = true
-        var decay = 0.5
+        var decay = 1.0 // XXX; tweak
         if (elementInfo.hip == true) {
             sprite.inputEnabled = false
             decay = 1.0
             sprite.body.collideWorldBounds = false
         }
         sprite.body.bounce.set(decay, decay)
+
+        if (reaction == undefined) {
+            sprite.alpha = 0
+            PHASER.add.tween(sprite).to({ alpha: 1 }, 500, Phaser.Easing.Linear.None, true, 0, 0, false)
+        }
 
         if (elementInfo.hip) {
             // high energy particle
@@ -162,6 +168,7 @@ playState.prototype = {
                 // decay object - don't if its already been removed my fusion
                 if (sprite.dontDecay == false) {
                     self.atoms.remove(sprite)
+                    self.incrementScore(elementInfo.decay.score)
                     var elementTypes = elementInfo.decay.elements
                     $.each(elementTypes, function (i) {
                         self.addElement(elementTypes[i], sprite.world.x, sprite.world.y)
@@ -180,66 +187,48 @@ playState.prototype = {
         var self = this
         // dont allow selection of hip's
         if (self.elementsInfo[sprite.elementType].selectable != false && self.selected.indexOf(sprite) == -1) {
+            sprite.bitmap.setHSL(0.2)
+            self.selected.push(sprite)
+            if (self.selected.length > 2) {
+                var deselect = self.selected.shift()
+                deselect.bitmap.setHSL(0.0)
+            }
             if (self.selected.length == 2) {
-                // XXX: deselect if its already selected
-            } else {
-                sprite.bitmap.setHSL(0.2)
-                self.selected.push(sprite)
-                if (self.selected.length > 2) {
-                    var deselect = self.selected.shift()
-                    deselect.bitmap.setHSL(0.0)
-                }
-                if (self.selected.length == 2) {
-                    // change velocity so its towards each other
-                    var a = self.selected[0]
-                    var b = self.selected[1]
-                    dx = a.world.x - b.world.x
-                    dy = a.world.y - b.world.y
-                    //console.log(a, b, dx, dy)
-                    // get the distance between the two points
-                    var h = Math.sqrt(dx * dx + dy * dy)
-                    var f = h / ACCELERATOR_SPEED                 
-                    a.body.velocity.set(
-                        -dx / f,
-                        -dy / f
-                    )
-                    b.body.velocity.set(
-                        dx / f,
-                        dy / f
-                    )
-                }
+                // change velocity so its towards each other
+                var a = self.selected[0]
+                var b = self.selected[1]
+                dx = a.world.x - b.world.x
+                dy = a.world.y - b.world.y
+                //console.log(a, b, dx, dy)
+                // get the distance between the two points
+                var h = Math.sqrt(dx * dx + dy * dy)
+                var f = h / ACCELERATOR_SPEED                 
+                a.body.velocity.set(
+                    -dx / f,
+                    -dy / f
+                )
+                b.body.velocity.set(
+                    dx / f,
+                    dy / f
+                )
             }
         }
     },
 
     collide: function (a, b) {
         var self = this
-        var aa = undefined
-        var bb = undefined
-        self.selected = self.selected.filter(function (e) {
-            if (e == a || e == b) {
-                e.bitmap.setHSL(0.0) // disable
-                if (e == a) aa = a
-                if (e == b) bb = b
-                return false
-            }
-            return true
-        })
 
-        // XXX: shforce chain reaction - note tweak the reaction speed
-        if (true) { //aa != undefined && bb != undefined) {
-            // they hit each other!
-            var cp = a.body.speed + b.body.speed
-            //console.log(a, 'hit', b, 'closing speed: ' + cp)
-            // 1.5 to account for rounding errors
-            if (cp >= REACTION_SPEED && self.allowedCombination(a, b)) {
-                // remove the sprites and replace with the combined
-                a.dontDecay = true
-                b.dontDecay = true
-                self.atoms.remove(a)
-                self.atoms.remove(b)
-                self.fuseElements(a, b)
-            }
+        // they hit each other!
+        var cp = a.body.speed + b.body.speed
+        //console.log(a, 'hit', b, 'closing speed: ' + cp)
+        // 1.5 to account for rounding errors
+        if (cp >= REACTION_SPEED && self.allowedCombination(a, b)) {
+            // remove the sprites and replace with the combined
+            a.dontDecay = true
+            b.dontDecay = true
+            self.atoms.remove(a)
+            self.atoms.remove(b)
+            self.fuseElements(a, b)
         }
     },
 
@@ -249,11 +238,20 @@ playState.prototype = {
         var x = a.world.x + ((a.world.x - b.world.x) / 2)
         var y = a.world.y + ((a.world.y - b.world.y) / 2)
         
+        // remove items from selected array
+        self.selected = self.selected.filter(function (e) {
+            if (e == a || e == b) {
+                e.bitmap.setHSL(0.0) // disable
+                return false
+            }
+            return true
+        })
+
         var fusionResult = self.getFusionResult(a, b)
-        self.score += fusionResult.score
+        self.incrementScore(fusionResult.score)
         var elementTypes = fusionResult.elements
         $.each(elementTypes, function (i) {
-            self.addElement(elementTypes[i], x, y)
+            self.addElement(elementTypes[i], x, y, true)
         })
     },
 
@@ -276,6 +274,11 @@ playState.prototype = {
             retval = self.combinations[b.elementType + '+' + a.elementType]
         }
         return retval
+    },
+
+    incrementScore: function (points) {
+        var self = this
+        self.score += points
     }
 }
 
